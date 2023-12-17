d35595cd276694e07df761c10a5c579c04d6b678
DialloAbdou <abdoudiallo3826@yahoo.fr>
2023-12-17 17:14:02 +0100

    ajout d'un parametre objet

---
+++ MiniApi/Personne.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using System.Reflection;
+using System.Security.Principal;
+
+namespace MiniApi
+{
+    public class Personne
+    {
+        public string Nom { get; set; } = string.Empty;
+        public string Prenom { get; set; } = string.Empty;
+
+        //public static bool TryParse(string value, out Personne? personne)
+        //{
+
+        //    try
+        //    {
+        //        var data = value.Split(' ');
+        //        personne = new Personne
+        //        {
+        //            Nom = data[0],
+        //            Prenom = data[1]
+        //        };
+
+        //        return true;
+        //    }
+        //    catch (Exception)
+        //    {
+
+        //        personne = null;
+        //        return false;
+        //    }
+        //}
+
+        public static async ValueTask<Personne> BindAsync(HttpContext context, ParameterInfo info)
+        {
+
+            try
+            {
+                using var StreamReader = new StreamReader(context.Request.Body);
+                var body = await StreamReader.ReadToEndAsync();
+                var data = body.Split(' ');
+               Personne personne = new Personne
+                {
+                    Nom = data[0],
+                    Prenom = data[1]
+                };
+
+                return personne;
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+    }
+}
--- MiniApi/Program.cs
+++ MiniApi/Program.cs
@@ -1,14 +1,32 @@
-﻿using MiniApi;
+﻿using Microsoft.AspNetCore.Mvc;
+using MiniApi;
+using System.Reflection.Metadata.Ecma335;
 
+List<Article> articles = new List<Article>()
+{
+    new Article(1, "Marteau"),
+    new Article(2, "Scie"),
+
+};
 var builder = WebApplication.CreateBuilder();
 var app = builder.Build();
 app.MapGet("/get", () => "Bonjour Get");
 app.MapPost("/post", () => "Bonjour Post");
 app.MapPut("/put", () => "Bonjour put");
 app.MapPatch("/patch", () => "Bonjour patch");
-app.MapGet("/articles/{id:int}", (int id) => new Article(id, "Marteau"));
+app.MapGet("/articles/{id:int}", (int id)=>
+{
+    var article = articles.Find(a=>a.Id == id);
+    if (article is not null) return Results.Ok(article);
+    return Results.BadRequest();
+});
 app.MapGet("/articles/{titre}", (string titre) => new Article(9999, "Marteau"));
+//app.MapGet("/personnes/{nom}", ([FromRoute]string nom,
+//                                [FromQuery] string? prenom,
+//                                [FromHeader(Name= "Accept-Encoding")] string encoding)=> Results.Ok($"{nom},{prenom} {encoding}"));
 
+//app.MapGet("/personne/indentite", (Personne p) => Results.Ok(p));
+app.MapPost("/personne/identite", (Personne p) => Results.Ok(p));
 app.Run();
 
 
