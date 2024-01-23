f499fa634e5b86ee9c386fb83e64a857e319cd25
DialloAbdou <abdoudiallo3826@yahoo.fr>
2024-01-23 22:22:12 +0100

    traiter les parametre spéciaux

--- MiniApi/Personne.cs
+++ MiniApi/Personne.cs
@@ -9,47 +9,59 @@ namespace MiniApi
         public string Nom { get; set; } = string.Empty;
         public string Prenom { get; set; } = string.Empty;
 
-        //public static bool TryParse(string value, out Personne? personne)
-        //{
-
-        //    try
-        //    {
-        //        var data = value.Split(' ');
-        //        personne = new Personne
-        //        {
-        //            Nom = data[0],
-        //            Prenom = data[1]
-        //        };
-
-        //        return true;
-        //    }
-        //    catch (Exception)
-        //    {
-
-        //        personne = null;
-        //        return false;
-        //    }
-        //}
-
-        public static async ValueTask<Personne> BindAsync(HttpContext context, ParameterInfo info)
+        /// <summary>
+        /// WITH GET
+        /// elle est permet de prendre un string 
+        /// en parametre et renvoyer et un objet 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="personne"></param>
+        /// <returns></returns>
+        public static bool TryParse(string value, out Personne? personne)
         {
 
             try
             {
-                using var StreamReader = new StreamReader(context.Request.Body);
-                var body = await StreamReader.ReadToEndAsync();
-                var data = body.Split(' ');
-               Personne personne = new Personne
+                var data = value.Split(' ');
+                personne = new Personne
                 {
                     Nom = data[0],
                     Prenom = data[1]
                 };
 
-                return personne;
+                return true;
             }
             catch (Exception)
             {
 
+                personne = null;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// With POST
+        /// elle permet de recevoir un fichier et elle retourne un objet 
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="info"></param>
+        /// <returns></returns>
+        public static async ValueTask<Personne> BindAsync(HttpContext context, ParameterInfo info)
+        {
+            try
+            {
+                using var StreamReader = new StreamReader(context.Request.Body);
+                var body = await StreamReader.ReadToEndAsync();
+                var data = body.Split(' ');
+                Personne personne = new Personne
+                {
+                    Nom = data[0],
+                    Prenom = data[1]
+                };
+                return personne;
+            }
+            catch (Exception)
+            {
                 return null;
             }
         }
--- MiniApi/Program.cs
+++ MiniApi/Program.cs
@@ -1,11 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MiniApi;
 using System.Reflection.Metadata.Ecma335;
-
 List<Article> articles = new List<Article>()
 {
     new Article(1, "Marteau"),
     new Article(2, "Scie"),
+    new Article(3, "Pointes"),
+
 
 };
 var builder = WebApplication.CreateBuilder();
@@ -14,19 +15,27 @@ app.MapGet("/get", () => "Bonjour Get");
 app.MapPost("/post", () => "Bonjour Post");
 app.MapPut("/put", () => "Bonjour put");
 app.MapPatch("/patch", () => "Bonjour patch");
-app.MapGet("/articles/{id:int}", (int id)=>
-{
-    var article = articles.Find(a=>a.Id == id);
-    if (article is not null) return Results.Ok(article);
-    return Results.BadRequest();
-});
-app.MapGet("/articles/{titre}", (string titre) => new Article(9999, "Marteau"));
-//app.MapGet("/personnes/{nom}", ([FromRoute]string nom,
-//                                [FromQuery] string? prenom,
-//                                [FromHeader(Name= "Accept-Encoding")] string encoding)=> Results.Ok($"{nom},{prenom} {encoding}"));
+app.MapMethods("/methods", new[] { "GET", "POST", "PUT", "DELETE" }, () => "hello Methods");
+app.MapGet("/article/1", () => new Article(1, "Portable"));
 
-//app.MapGet("/personne/indentite", (Personne p) => Results.Ok(p));
+app.MapGet("/articles/{id:int}", (int id) =>
+{
+    var article = articles.Find(x => x.Id == id);
+    if (article is not null) return Results.Ok(article);
+    return Results.NotFound();
+});
+app.MapGet("/articles/{title}", (string title) => new Article(999, "Portable"));
+
+app.MapGet("/personnes/{nom}",
+          ([FromRoute] string nom,
+          [FromQuery] string? prenom,
+          [FromHeader(Name = "Accept-Encoding")] string encoding) =>
+            Results.Ok($"{nom} {prenom} {encoding}"));
+
+//app.MapGet("/personne/identite", (Personne p) => Results.Ok(p));
+
 app.MapPost("/personne/identite", (Personne p) => Results.Ok(p));
+
 app.Run();
 
 
