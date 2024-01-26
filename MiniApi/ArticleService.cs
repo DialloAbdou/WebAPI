38e8f6ce524751cbaeb839f3f5ad7f7f342d1dfa
DialloAbdou <abdoudiallo3826@yahoo.fr>
2024-01-26 11:53:28 +0100

    ajout de gestion de service

---
+++ MiniApi/ArticleService.cs
@@ -0,0 +1,22 @@
+﻿namespace MiniApi
+{
+    public class ArticleService
+    {
+        private List<Article> _articles = new List<Article>()
+        {
+              new Article(1, "Marteau"),
+              new Article(2, "Scie")
+
+        };
+
+        public List<Article> GetAllArticles() => _articles;
+
+        public Article AddArticle(string title)
+        {
+            var article=  new Article(_articles.Max(a=>a.Id)+1, title);
+            _articles.Add(article);
+            return article;
+        }
+       
+    }
+}
--- MiniApi/Program.cs
+++ MiniApi/Program.cs
@@ -1,15 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MiniApi;
 using System.Reflection.Metadata.Ecma335;
-List<Article> articles = new List<Article>()
-{
-    new Article(1, "Marteau"),
-    new Article(2, "Scie"),
-    new Article(3, "Pointes"),
-
-
-};
 var builder = WebApplication.CreateBuilder();
+builder.Services.AddSingleton<ArticleService>();
 var app = builder.Build();
 app.MapGet("/get", () => "Bonjour Get");
 app.MapPost("/post", () => "Bonjour Post");
@@ -18,11 +11,17 @@ app.MapPatch("/patch", () => "Bonjour patch");
 app.MapMethods("/methods", new[] { "GET", "POST", "PUT", "DELETE" }, () => "hello Methods");
 app.MapGet("/article/1", () => new Article(1, "Portable"));
 
-app.MapGet("/articles/{id:int}", (int id) =>
+app.MapGet("/articles/{id:int}", (int id, ArticleService service) =>
 {
-    var article = articles.Find(x => x.Id == id);
+    var article = service.GetAllArticles().Find(x => x.Id == id);
     if (article is not null) return Results.Ok(article);
     return Results.NotFound();
+});
+
+app.MapPost("/articles/", (Article a, ArticleService service) =>
+{
+    var result = service.AddArticle(a.Title);
+     return Results.Ok(result); 
 });
 app.MapGet("/articles/{title}", (string title) => new Article(999, "Portable"));
 
